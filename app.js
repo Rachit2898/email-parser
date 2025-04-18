ea1ca54d76e34315149ebb38a52b18950800ecfc
Rachit2898 <merachit2898@gmail.com>
2025-04-18 19:10:23 +0530

    code updated

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ import { MailListener } from "mail-listener5";
 const app = express();
 app.use(express.json());
 
+app.use("/",(req,res)=>{
+  res.status(200).json({success:"true"})
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
 // Handle uncaught exceptions (sync)
 process.on("uncaughtException", (err) => {
   console.error("Uncaught Exception:", err);
